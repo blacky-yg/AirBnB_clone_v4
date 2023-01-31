57a95842c8570d31c79819aa4c2349a750239fda
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-31 01:29:33 +0100

    [init]: first commit

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This module contains the places_amenities view """
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
